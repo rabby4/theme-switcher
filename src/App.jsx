ec7ad49eb5171929f7a77f77df74f313f2f2bdc5
rabby4 <mdrabbybd4747@gmail.com>
2024-01-01 01:38:04 +0600

    create theme provider with use context

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 
+import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './context/theme'
 
 function App() {
+  const [themeMode, setThemeMode] = useState()
 
   return (
     <>
+      <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              {/* theme btn */}
+            </div>
 
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* theme btn */}
-          </div>
-
-          <div className="w-full max-w-sm mx-auto">
-            {/* Card */}
+            <div className="w-full max-w-sm mx-auto">
+              {/* Card */}
+            </div>
           </div>
         </div>
-      </div>
+      </ThemeProvider>
+
 
     </>
   )
