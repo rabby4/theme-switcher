85c00e74864a0f9601f82f169e138bdaaaca3783
rabby4 <mdrabbybd4747@gmail.com>
2024-01-01 01:43:35 +0600

    get html classlist for theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './context/theme'
 
 function App() {
-  const [themeMode, setThemeMode] = useState()
+  const [themeMode, setThemeMode] = useState('light')
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
     <>
