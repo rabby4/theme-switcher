221738bab07c66aefb7bc862a3ba6ebe59b61982
rabby4 <mdrabbybd4747@gmail.com>
2024-01-01 01:55:35 +0600

    create card and toggle button and add toggle button function

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './context/theme'
+import ThemeButton from './components/ThemeButton'
+import Card from './components/Card'
 
 function App() {
   const [themeMode, setThemeMode] = useState('light')
@@ -23,11 +25,11 @@ function App() {
         <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
             <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-              {/* theme btn */}
+              <ThemeButton></ThemeButton>
             </div>
 
             <div className="w-full max-w-sm mx-auto">
-              {/* Card */}
+              <Card></Card>
             </div>
           </div>
         </div>
